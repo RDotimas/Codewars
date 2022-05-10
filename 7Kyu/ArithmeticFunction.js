9bea8fa10ed7032659c6b9f2ea89c05a94a0fee9
Dots <dotimas@hotmail.com>
2022-05-09 21:30:13 -0400

    ArithemeticFunction

---
+++ 7Kyu/ArithmeticFunction.js
@@ -0,0 +1,19 @@
+//Make a function that does arithmetic!
+//https://www.codewars.com/kata/583f158ea20cfcbeb400000a
+
+function arithmetic(a, b, operator){
+    switch (operator){
+      case operator = 'add':
+        return a + b
+      break;
+      case operator = 'subtract':
+        return a - b
+      break;
+      case operator = 'multiply':
+        return a * b
+      break;
+      case operator = 'divide':
+        return a / b
+      break;
+      }
+  }
