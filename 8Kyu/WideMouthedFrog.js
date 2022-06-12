675fff008dd981dfd8252c1ebb56548cd0999b5f
Dots <ralphydot@gmail.com>
2022-06-12 04:18:19 -0400

    WideMouthedFrog

---
+++ 8Kyu/WideMouthedFrog.js
@@ -0,0 +1,7 @@
+//Wide Mouthed Frog
+//https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+
+function mouthSize(animal) {
+    let ani = animal.toLowerCase()
+    return (ani === 'alligator') ? 'small' : 'wide'
+  }
