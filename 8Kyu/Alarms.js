159a0c142d5f5dfb75b83fc8506940032d030a44
Dots <ralphydot@gmail.com>
2022-06-19 23:33:46 -0400

    Alarms

---
+++ 8Kyu/Alarms.js
@@ -0,0 +1,8 @@
+// Alarm Solutions
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038/javascript
+
+function setAlarm(employed, vacation){
+    if (employed === true && vacation === false)
+      return true
+    else return false
+  }
