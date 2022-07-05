ace9f1aaf77f82c33c8e9a0756c6e95a2b580cea
Dots <ralphydot@gmail.com>
2022-07-04 23:28:27 -0400

    CountSheep

---
+++ 8Kyu/CountSheep.js
@@ -0,0 +1,9 @@
+//If you can't sleep count sheep
+//https://www.codewars.com/kata/5b077ebdaf15be5c7f000077
+
+var countSheep = function (num){
+    let str = '';
+    for (let i = 1; i <= num; i++) {
+      str += `${i} sheep...`
+    }return str;
+  }
