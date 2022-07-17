e8f1461aa1af425045df1e0021ddfb48bc9c228e
Dots <ralphydot@gmail.com>
2022-07-16 23:51:47 -0400

    TrainingSwitch

---
+++ 8Kyu/TrainingSwitch.js
@@ -0,0 +1,10 @@
+//Training Switch
+//https://www.codewars.com/kata/572059afc2f4612825000d8a/javascript
+
+function howManydays(month){
+    switch ( month ){
+      case 2 : return 28
+      case 4: case 6: case 9: case 11: return 30
+      default: return 31
+    }
+  }
