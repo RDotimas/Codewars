f7ee3b90a8fe3390b7e9a3d8a291cef4186c0282
Dots <ralphydot@gmail.com>
2022-07-23 21:08:46 -0400

    RepeatIt

---
+++ 8Kyu/RepeatIt.js
@@ -0,0 +1,7 @@
+//Repeat it
+//https://www.codewars.com/kata/557af9418895e44de7000053/javascript
+
+var repeatIt = function(str, n) {
+    if (typeof str !== 'string') return 'Not a string'
+  return str.repeat(n)
+  }
