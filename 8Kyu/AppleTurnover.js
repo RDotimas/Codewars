d268b7e8cb0db92fca098bef5fb93cb5f431118e
Dots <ralphydot@gmail.com>
2022-08-05 23:27:39 -0400

    AppleTurnover

---
+++ 8Kyu/AppleTurnover.js
@@ -0,0 +1,6 @@
+//Apple Turnover
+//https://www.codewars.com/kata/580a094553bd9ec5d800007d/train/javascript
+
+function apple(x){
+    return (Math.pow(x,2) >= 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.')
+  }
