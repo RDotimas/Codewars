bd5fe3dba9395d7524180b95f0e1720938f5ad44
Dots <ralphydot@gmail.com>
2022-08-08 23:02:37 -0400

    FillArrayP1

---
+++ 8Kyu/FillArrayP1.js
@@ -0,0 +1,11 @@
+//Fill an array part 1
+//https://www.codewars.com/kata/571d42206414b103dc0006a1/javascript
+
+function arr(N) {
+    let ans = []
+    for(let i = 0; i < N; i++){
+      ans.push(i)
+  
+    }
+    return ans
+  }
