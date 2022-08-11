d9095286a9371f47d7f6222606933ecdaa5b28e2
Dots <ralphydot@gmail.com>
2022-08-11 18:29:07 -0400

    RegBallSuperBall

---
+++ 8Kyu/RegBallSuperBall.js
@@ -0,0 +1,6 @@
+//Regular ball Super ball
+//https://www.codewars.com/kata/53f0f358b9cb376eca001079/train/javascript
+
+var Ball = function(ballType) {
+    this.ballType = ballType || 'regular'
+  };
