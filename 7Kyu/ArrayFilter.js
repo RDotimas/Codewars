20fa79aec7a16c08177e0b2b7fe2422969f7ed26
Dots <ralphydot@gmail.com>
2022-08-18 04:25:02 -0400

    ArrayFilter

---
+++ 7Kyu/ArrayFilter.js
@@ -0,0 +1,6 @@
+//Array Filter
+//https://www.codewars.com/kata/514a6336889283a3d2000001/train/javascript
+
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(item => item % 2 === 0)                        
+  }
