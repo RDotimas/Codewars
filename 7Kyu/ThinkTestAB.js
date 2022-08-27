d428147a65f655ffff87674d10a62acdd1bb7bed
Dots <ralphydot@gmail.com>
2022-08-26 22:17:24 -0400

    ThinkTestAB.js

---
+++ 7Kyu/ThinkTestAB.js
@@ -0,0 +1,7 @@
+//Thinking and testing: A and B
+//https://www.codewars.com/kata/56d904db9963e9cf5000037d/train/javascript
+
+function testit(a,b){
+    console.log (a,b)
+      return a | b;
+    }
