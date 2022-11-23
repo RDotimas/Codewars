0f5a4b062bbefe5f7286951d8157e17c627e0fd6
Dots <ralphydot@gmail.com>
2022-11-23 00:01:50 -0500

    ArrowFunctionsOdd

---
+++ 8Kyu/ArrowFunctionsOdd.js
@@ -0,0 +1,8 @@
+//Are Arrow Functions Odd?
+//https://www.codewars.com/kata/559f80b87fa8512e3e0000f5/train/javascript
+
+
+function odds(values){
+    // arrow it
+    return values.filter( n => n % 2 !== 0);
+  }
