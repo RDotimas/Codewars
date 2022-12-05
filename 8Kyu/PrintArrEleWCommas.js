d253fa6019eab0bbd737e6e15b606f3a3264fb30
Dots <ralphydot@gmail.com>
2022-12-04 23:49:32 -0500

    PrintArrEleWCommas

---
+++ 8Kyu/PrintArrEleWCommas.js
@@ -0,0 +1,6 @@
+//Printer array elements with comma delimiters
+//https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+
+function printArray(array){
+    return array.toString()
+  }
